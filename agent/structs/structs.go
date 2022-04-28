5b7e0802a995f71adb988969bf714c7f260ea1e0
Riddhi Shah <riddhi@hashicorp.com>
2022-04-28 07:41:54 -0700

    Validate port on mesh service registration
    Without a port in the registration, the mesh is not functional.
    For agentless, since there will not be any client agents for auto port assignment,
    require a port to be specified on registration of a mesh service
    (connect proxy, gateway types, sidecar definitions, connect native services).

--- agent/structs/structs.go
+++ agent/structs/structs.go
@@ -1333,9 +1333,8 @@ func (s *NodeService) Validate() error {
 					"services"))
 		}
 
-		if s.Port == 0 && s.SocketPath == "" {
-			result = multierror.Append(result, fmt.Errorf(
-				"Port or SocketPath must be set for a Connect proxy"))
+		if s.Port == 0 {
+			result = multierror.Append(result, fmt.Errorf("Port must be non-zero for a %s", s.Kind))
 		}
 
 		if s.Connect.Native {
@@ -1464,6 +1463,16 @@ func (s *NodeService) Validate() error {
 					"A SidecarService cannot have a nested SidecarService"))
 			}
 		}
+
+		if s.Connect.SidecarService.Port == 0 {
+			result = multierror.Append(result, fmt.Errorf(
+				"Side car service must have non-zero port"))
+		}
+	}
+
+	if s.Connect.Native && s.Port == 0 {
+		result = multierror.Append(result, fmt.Errorf(
+			"Connect native service must have non-zero port"))
 	}
 
 	return result
